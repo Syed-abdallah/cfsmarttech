0db7240fdd8c3c8ba82d64a2340db4f8b869f286
Syed Abdullah <myselfsyedabdullah@gmail.com>
2025-05-19 08:47:30 +0500

    prices seeder

---
+++ database/seeders/PropertySizeSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PropertySizeSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('property_sizes')->insert([
+            [
+                'size' => 1200,
+                'price' => 15,
+                'created_at' => '2025-05-17 18:15:25',
+                'updated_at' => '2025-05-18 17:11:45'
+            ],
+            [
+                'size' => 1800,
+                'price' => 12,
+                'created_at' => '2025-05-17 09:33:49',
+                'updated_at' => '2025-05-18 17:12:07'
+            ],
+            [
+                'size' => 2200,
+                'price' => 12,
+                'created_at' => '2025-05-17 16:01:36',
+                'updated_at' => '2025-05-18 17:12:10'
+            ],
+            [
+                'size' => 3000,
+                'price' => 30,
+                'created_at' => '2025-05-17 09:33:49',
+                'updated_at' => '2025-05-18 17:12:18'
+            ]
+        ]);
+    }
+}
