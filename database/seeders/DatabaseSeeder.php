fc64027f94186fb67bb0879531f32618a5471039
Syed Abdullah <myselfsyedabdullah@gmail.com>
2025-05-21 14:40:27 +0500

    major updates

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,13 +15,13 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        $this->call([
-            PackageTypeSeeder::class,
-               AdditionalCostSeeder::class,
-                        PropertySizeSeeder::class,
-                        RoomTypeSeeder::class,
-        PermissionSeeder::class,
-        ]);
-     
+        // $this->call([
+        //     PackageTypeSeeder::class,
+        //        AdditionalCostSeeder::class,
+        //                 PropertySizeSeeder::class,
+        //                 RoomTypeSeeder::class,
+        // PermissionSeeder::class,
+        // ]);
+         $this->call(SuperAdminRoleSeeder::class);
     }
 }
